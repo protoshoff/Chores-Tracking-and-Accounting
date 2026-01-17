bf3e97ecf0beed9d90fad88a66fad7539756ff54
protoshoff <mike@garbago.com>
2026-01-16 20:31:17 -0700

    fix(kiosk): Temporarily disable QtMultimedia/SoundService to diagnose startup hang

--- kiosk/services/sound.py
+++ kiosk/services/sound.py
@@ -1,4 +1,5 @@
-from PySide6.QtMultimedia import QSoundEffect
+```python
+# from PySide6.QtMultimedia import QSoundEffect
 from PySide6.QtCore import QUrl
 import os
 
@@ -8,19 +9,8 @@ class SoundService:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(SoundService, cls).__new__(cls)
-            cls._instance._init()
+            cls._instance.initialized = False
         return cls._instance
-        
-    def _init(self):
-        self.effects = {}
-        base_path = os.path.dirname(os.path.abspath(__file__))
-        self.assets_path = os.path.join(base_path, "../assets/sounds")
-        
-        # Preload common sounds
-        self._load("click", "click.wav")
-        self._load("success", "success.wav")
-        self._load("error", "error.wav")
-        
     def _load(self, name, filename):
         path = os.path.join(self.assets_path, filename)
         if os.path.exists(path):
